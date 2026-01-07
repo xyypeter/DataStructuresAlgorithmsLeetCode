7836ad84e0b6b90d0533156d8b8aa6612d09f2a8
钰 刘 <18391128562@163.com>
2026-01-07 15:10:24 +0800

    1.使用新的数据结构与算法题目，测试Git的提交功能

---
+++ 011_二叉树的层次遍历/011_二叉树的层次遍历.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+
+	}
+};
+
+int main()
+{
+
+}
+
+
