aab11b77a4fe5d75477a598e3b88f37d616f55eb
钰 刘 <18391128562@163.com>
2026-01-12 11:17:54 +0800

    使用回溯法，每一层选择一个未使用的数字加入当前路径，当路径长度等于数组长度时得到一个排列，通过撤销选择遍历所有可能情况

---
+++ 015_全排列问题/015_全排列问题.cpp
@@ -0,0 +1,44 @@
+﻿#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    vector<bool> used;
+    void backtrack(vector<int>& nums) {
+        //递归终止条件:排列长度等于nums长度
+        if (path.size() == nums.size()) {
+            result.push_back(path);
+            return;
+        }
+        //选择列表
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtrack(nums);        //进入下一层
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        used.resize(nums.size(), false);
+        backtrack(nums);
+        return result;
+    }
+};
+
+int main(){
+    vector<int> nums = { 1, 2, 3 };
+    Solution s;
+    vector<vector<int>> ans = s.permute(nums);
+    for (auto& v : ans) {
+        for (int x : v) cout << x << " ";
+        cout << endl;
+    }
+    return 0;
+}
