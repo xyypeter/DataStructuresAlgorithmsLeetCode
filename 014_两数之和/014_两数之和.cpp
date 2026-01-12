c1052a48e2394efed81053b316a865999ce6a244
钰 刘 <18391128562@163.com>
2026-01-12 10:28:44 +0800

    使用哈希表存储已遍历的元素及其下标，在遍历数组时查找目标值与当前元素的差是否存在，从而在 O(n) 时间内完成查找。

---
+++ 014_两数之和/014_两数之和.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            int need = target - nums[i];
+            if (mp.count(need)) return { mp[need],i };
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    vector<int> nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    Solution s;
+    vector<int> result = s.twoSum(nums, target);
+
+    cout << result[0] << " " << result[1] << endl; // 输出 0 1
+    return 0;
+}
