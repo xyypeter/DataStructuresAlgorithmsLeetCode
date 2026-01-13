2abb35e78b4a82b0ee553c4968ddeed5a218364f
钰 刘 <18391128562@163.com>
2026-01-13 09:29:11 +0800

    利用 nums1 尾部的空余空间，采用三个指针从后向前比较并填充元素，实现原地合并两个有序数组，时间复杂度 O(m+n)，空间复杂度 O(1)

---
+++ 016_合并两个有序数组/016_合并两个有序数组.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;            //nums1有效部分末尾
+        int j = n - 1;            //nums2末尾
+        int k = m + n - 1;        //nums1总末尾
+        while (i >= 0 && j >= 0) {//从后向前合并
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
+
+int main(){
+    vector<int> nums1 = { 1,2,3,0,0,0 };
+    vector<int> nums2 = { 2,5,6 };
+    Solution s;
+    s.merge(nums1, 3, nums2, 3);
+    for (int x : nums1) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+
