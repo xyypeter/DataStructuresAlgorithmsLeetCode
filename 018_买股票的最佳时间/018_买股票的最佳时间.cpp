7c8dd7c54147b11dce23a20fda202c3594711523
钰 刘 <18391128562@163.com>
2026-01-15 13:56:55 +0800

    一次遍历，记录最低买入价，在每一天尝试卖出并更新最大利润

---
+++ 018_买股票的最佳时间/018_买股票的最佳时间.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;//记录历史最低价
+        int maxProfit = 0;     //记录最大利润
+        for (int price : prices) {
+            //更新最低买入价
+            minPrice = min(minPrice, price);
+            //更新最大利润
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> prices = { 7, 1, 5, 3, 6, 4 };
+    cout << "最大利润为: " << s.maxProfit(prices) << endl;
+    return 0;
+}
+
+
