8e41d4c46eae89d67084e5ae9e2317fa34f85ebf
钰 刘 <18391128562@163.com>
2026-01-16 10:54:19 +0800

    BFS 保证层次，方向标志控制顺序，插入位置决定锯齿

---
+++ 021_二叉树锯齿形层序遍历/021_二叉树锯齿形层序遍历.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == nullptr) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftOrRight = true;//用于控制方向
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                int index = leftOrRight ? i : size - 1 - i;
+                level[index] = node->val;
+                if (node->left) q.push(node->left);
+                if (node->right)q.push(node->right);
+            }
+            result.push_back(level);
+            leftOrRight = !leftOrRight;
+        }
+        return result;
+    }
+};
+
+int main() {
+    /*
+            3
+           / \
+          9  20
+             / \
+            15  7
+    */
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    Solution s;
+    vector<vector<int>> res = s.zigzagLevelOrder(root);
+    cout << "锯齿形层序遍历结果：" << endl;
+    for (auto& level : res) {
+        for (int val : level)
+            cout << val << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
