6657ac448f7d732b0d69e64349ec971bf13804d9
钰 刘 <18391128562@163.com>
2026-01-27 17:20:26 +0800

    删除有序链表重复元素 核心解决办法:用一个“虚拟头结点 + 双指针”，一旦发现某个值重复，就把这一整段全部跳过,但是需要注意的是，重复的元素是否保留的问题，本题重复元素会全部删除

---
+++ 035_删除链表中的重复元素II/035_删除链表中的重复元素II.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;      //使用虚拟头结点方便处理头结点被删除的情况
+        ListNode* curr = head;  //curr用来遍历链表
+        ListNode* prev = &dummy;//prev指向"已确认且不重复"的最后一个节点
+        while (curr) {
+            if (curr->next && curr->val == curr->next->val) {
+                int duplicateVal = curr->val;
+                while (curr && curr->val == duplicateVal) {
+                    curr = curr->next;
+                }
+                prev->next = curr;
+            }else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return dummy.next;
+    }
+};
+
+int main(){
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next->next = new ListNode(5);
+    Solution s;
+    ListNode* reversed = s.deleteDuplicates(head);
+    ListNode* p = reversed;
+    while (p != nullptr) {
+        cout << p->val << "->";
+        p = p->next;
+    }
+    cout << endl;
+    return 0;
+}
