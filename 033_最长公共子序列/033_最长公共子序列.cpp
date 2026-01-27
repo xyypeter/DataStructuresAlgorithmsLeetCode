d15eef450865aec78bcf121083fddb5cfc7191ee
钰 刘 <18391128562@163.com>
2026-01-27 10:45:15 +0800

    LCS_DP算法总结 1.字符相等，看左上 2.字符不等，看左或上

---
+++ 033_最长公共子序列/033_最长公共子序列.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        //dp[i][j]: text1 前 i 个字符 与 text2 前 j 个字符的 LCS 长度
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main(){
+    Solution s;
+    string text1 = "abcde";
+    string text2 = "ace";
+    cout << s.longestCommonSubsequence(text1, text2) << endl; // 输出 3
+    return 0;
+}
+
