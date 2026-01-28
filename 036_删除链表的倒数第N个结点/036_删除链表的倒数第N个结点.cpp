4d8a04835da5e5f0620d37be7334308fb366c907
钰 刘 <18391128562@163.com>
2026-01-28 09:49:20 +0800

    关键点如下 1.倒数第 n 个 → 快慢指针 2.涉及删除头节点 → dummy 3.删除的是 slow->next

---
+++ 036_删除链表的倒数第N个结点/036_删除链表的倒数第N个结点.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // 虚拟头结点
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* slow = &dummy;
+        ListNode* fast = &dummy;
+        // fast 先走 n 步
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+        // fast 和 slow 一起走
+        while (fast->next != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        // 删除倒数第 n 个节点
+        ListNode* toDelete = slow->next;
+        slow->next = slow->next->next;
+        delete toDelete; 
+        return dummy.next;
+    }
+};
+
+int main(){
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    Solution s;
+    ListNode* reversed = s.removeNthFromEnd(head,2);
+    ListNode* p = reversed;
+    while (p != nullptr) {
+        cout << p->val << "->";
+        p = p->next;
+    }
+    cout << endl;
+    return 0;
+    
+}
+
