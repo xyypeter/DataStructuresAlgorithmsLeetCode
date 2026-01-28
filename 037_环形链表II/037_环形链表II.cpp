42d602fa6c26dea0b15b4d74b40caf12be12cf39
钰 刘 <18391128562@163.com>
2026-01-28 10:15:10 +0800

    关键点总结: 1.快慢指针可以判断环 2.相遇后，一个从 head，一个从相遇点，同速前进 3.再次相遇点 = 入环点

---
+++ 037_环形链表II/037_环形链表II.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) { //第一步判断是否存在环
+            slow = slow->next;
+            fast = fast->next->next; 
+            if (slow == fast) {      //相遇表示存在环
+                ListNode* p1 = head;
+                ListNode* p2 = slow;
+                while (p1 != p2) {
+                    p1 = p1->next;
+                    p2 = p2->next;
+                }
+                return p1;
+            }
+        }
+        return nullptr;
+    }
+};
+
+int main(){
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
+    Solution s;
+    ListNode* reversed = s.detectCycle(head);
+    ListNode* p = reversed;
+    if (p != nullptr) {
+        cout << "链表有环";
+    }else {
+        cout << "链表无环";
+    }
+    return 0;
+}
+
